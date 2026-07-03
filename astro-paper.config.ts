c328ec77625853aff573c73d8663f9e007b250fa
BistuilderH <2931628779@qq.com>
2026-07-03 11:01:58 +0800

    init: AstroPaper blog with GitHub Pages deploy workflow

---
+++ astro-paper.config.ts
@@ -0,0 +1,43 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://bistuilderh.github.io/",
+    title: "BistuilderH's Blog",
+    description: "记录我的学习与思考。",
+    author: "BistuilderH",
+    profile: "https://github.com/BistuilderH",
+    ogImage: "default-og.jpg",
+    lang: "zh",
+    timezone: "Asia/Shanghai",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: true,
+      url: "https://github.com/BistuilderH/BistuilderH.github.io/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github",   url: "https://github.com/BistuilderH" },
+    { name: "mail",     url: "mailto:2931628779@qq.com" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
